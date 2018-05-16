df467be56319ad4036b8cc7b0943c294caeb5ee6
Tuomas Haapala <tuomas.haapala@anders.fi>
2018-05-16 23:12:17 +0300

    Implement signup POSTing

--- signups/api.py
+++ signups/api.py
@@ -1,4 +1,7 @@
+from django.utils.timezone import now
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import permissions, serializers, viewsets
+from rest_framework.exceptions import APIException, NotFound
 
 from .models import Signup, SignupTarget
 
@@ -25,6 +28,12 @@ class SignupSerializer(serializers.ModelSerializer):
         fields = ('id', 'target', 'created_at', 'cancelled_at')
 
 
+class SignupAlreadyExists(APIException):
+    status_code = 409
+    default_detail = _('The sign-up already exists.')
+    default_code = 'signup_already_exists'
+
+
 class SignupViewSet(viewsets.ModelViewSet):
     queryset = Signup.objects.select_related('target')
     serializer_class = SignupSerializer
@@ -32,3 +41,18 @@ class SignupViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        queryset = Signup.objects.filter(user=self.request.user, target=serializer.validated_data['target'])
+
+        if queryset.exists():
+            raise SignupAlreadyExists()
+
+        serializer.save(user=self.request.user)
+
+    def perform_destroy(self, instance):
+        if instance.cancelled_at:
+            raise NotFound()
+
+        instance.cancelled_at = now()
+        instance.save(update_fields=('cancelled_at',))
--- signups/tests/test_signup_api.py
+++ signups/tests/test_signup_api.py
@@ -1,7 +1,9 @@
+from django.utils.timezone import now
 from rest_framework.reverse import reverse
 
-from signups.tests.utils import check_disallowed_methods, get
+from signups.models import Signup
 from signups.tests.factories import SignupFactory
+from signups.tests.utils import check_disallowed_methods, delete, get, post
 
 LIST_URL = reverse('signup-list')
 
@@ -45,3 +47,58 @@ def test_user_can_see_only_own_signups(user_api_client, signup):
     assert results[0]['id'] == signup.id
 
     get(user_api_client, get_detail_url(other_user_signup), status_code=404)
+
+
+def test_post_signup(user_api_client, signup_target):
+    assert Signup.objects.count() == 0
+
+    post(user_api_client, LIST_URL, {'target': signup_target.identifier})
+
+    assert Signup.objects.count() == 1
+    new_signup = Signup.objects.latest('id')
+    assert new_signup.user == user_api_client.user
+    assert new_signup.target == signup_target
+    assert new_signup.cancelled_at is None
+    assert new_signup.created_at
+
+
+def test_post_signup_nonexisting_target(user_api_client):
+    assert Signup.objects.count() == 0
+
+    data = post(user_api_client, LIST_URL, {'target': 'foobar'}, status_code=400)
+    assert 'target' in data
+
+
+def test_post_already_existing_signup(user_api_client, signup, signup_target):
+    assert Signup.objects.count() == 1
+
+    post(user_api_client, LIST_URL, {'target': signup_target.identifier}, status_code=409)
+
+    assert Signup.objects.count() == 1
+
+
+def test_delete_signup(user_api_client, signup):
+    assert Signup.objects.count() == 1
+    assert Signup.objects.filter(cancelled_at=None).count() == 1
+
+    delete(user_api_client, get_detail_url(signup))
+
+    assert Signup.objects.count() == 1
+    assert Signup.objects.filter(cancelled_at=None).count() == 0
+
+    signup.refresh_from_db()
+    assert signup.cancelled_at
+
+
+def test_cannot_delete_other_user_signup(user_api_client):
+    other_user_signup = SignupFactory()
+
+    delete(user_api_client, get_detail_url(other_user_signup), status_code=404)
+
+    Signup.objects.get(id=other_user_signup.id)
+
+
+def test_cannot_delete_already_cancelled_signup(user_api_client):
+    signup = SignupFactory(user=user_api_client.user, cancelled_at=now())
+
+    delete(user_api_client, get_detail_url(signup), status_code=404)
