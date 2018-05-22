46abefd427d9898d5f18d792b42dae97fc69b3fc
Tuomas Haapala <tuomas.haapala@anders.fi>
2018-05-22 10:30:49 +0300

    Add settings for Tunnistamo oidc authentication

--- open_city_signups/urls.py
+++ open_city_signups/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include(router.urls)),
     path('docs/', include_docs_urls(title='Open City sign-ups')),
+    path('accounts/', include('allauth.urls')),
 ]
